3781b1d4f0ee08b33cadc5f0e743d368cd4f08de
Elio Monaco <elio.monaco@proton.me>
2023-06-30 00:42:39 +0200

    test

--- sensors/src/main.rs
+++ sensors/src/main.rs
@@ -1,54 +1,72 @@
+//https://github.com/emilk/egui/blob/master/examples
+
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+
 use eframe::egui;
+use std::thread;
+use std::time::Duration;
+use systemstat::{System, Platform, saturating_sub_bytes};
 
-fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options: eframe = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(220.0, 140.0)),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Confirm exit",
-        options,
-        Box::new(|_cc| Box::<MyApp>::default()),
-    )
-}
+fn sensor_data() -> Result<f32, &'static str>{
+    let sys = System::new();
 
-#[derive(Default)]
-struct MyApp {
-    allowed_to_close: bool,
-    show_confirmation_dialog: bool,
-}
-
-impl eframe::App for MyApp {
-    fn on_close_event(&mut self) -> bool {
-        self.show_confirmation_dialog = true;
-        self.allowed_to_close
+    match sys.cpu_temp() {
+        Ok(cpu_temp) => Ok(cpu_temp),
+        Err(x) => println!("CPU temp: {}", x)
     }
 
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Try to close the window");
-        });
-
-        if self.show_confirmation_dialog {
-            // Show confirmation dialog:
-            egui::Window::new("Do you want to quit?")
-                .collapsible(false)
-                .resizable(false)
-                .show(ctx, |ui| {
-                    ui.horizontal(|ui| {
-                        if ui.button("Cancel").clicked() {
-                            self.show_confirmation_dialog = false;
-                        }
-
-                        if ui.button("Yes!").clicked() {
-                            self.allowed_to_close = true;
-                            frame.close();
-                        }
-                    });
-                });
-        }
-    }
 }
+
+fn main() {
+    sensor_data()
+}
+
+// fn main() -> Result<(), eframe::Error> {
+//     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+//     let options = eframe::NativeOptions {
+//         decorated: false,
+//         transparent: true,
+//         initial_window_size: Some(egui::vec2(100.0, 120.0)),
+//         initial_window_pos: Some(egui::pos2(10.0, 10.0)),
+//         ..Default::default()
+//     };
+//     eframe::run_native(
+//         "Sensors",
+//         options,
+//         Box::new(|_cc| Box::<MyApp>::default()),
+//     )
+// }
+
+// struct MyApp {
+//     cpu_temp: String,
+//     age: u32,
+// }
+
+// impl Default for MyApp {
+//     fn default() -> Self {
+//         Self {
+//             //cpu_temp: "Arthur".to_owned(),
+//             cpu_temp: sys_info(),
+//             age: 42,
+//         }
+//     }
+// }
+
+// impl eframe::App for MyApp {
+//     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+//         egui::CentralPanel::default().show(ctx, |ui| {
+//             //ui.heading("My egui Application");
+//             ui.horizontal(|ui| {
+//                 let name_label = ui.label("Your name: ");
+//                 // ui.text_edit_singleline(&mut self.name)
+//                 //     .labelled_by(name_label.id);
+//             });
+//             // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+//             // if ui.button("Click each year").clicked() {
+//             //     self.age += 1;
+//             // }
+//             // ui.label(format!("Hello '{}', age {}", self.name, self.age));
+//         });
+//     }
+// }
