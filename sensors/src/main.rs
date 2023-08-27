0c6115af35181e192c0af44ac3688b6409c621ac
Elio Monaco <elio.monaco@proton.me>
2023-08-27 13:51:16 +0200

    test

--- sensors/src/main.rs
+++ sensors/src/main.rs
@@ -9,15 +9,37 @@ use std::thread;
 use std::time::Duration;
 use systemstat::{System, Platform, saturating_sub_bytes};
 
-fn sensor_data() -> Result<f32, &'static str>{
+// fn sensor_data() -> Result<f32, &'static str>{
+//     let sys = System::new();
+
+//     match sys.cpu_temp() {
+//         Ok(cpu_temp) => Ok(cpu_temp),
+//         Err(x) => println!("CPU temp: {}", x)
+//     }
+
+// }
+
+fn sensor_data() {
     let sys = System::new();
 
+    match sys.cpu_load_aggregate() {
+        Ok(cpu)=> {
+            println!("\nMeasuring CPU load...");
+            thread::sleep(Duration::from_secs(1));
+            let cpu = cpu.done().unwrap();
+            println!("CPU load: {}% user, {}% nice, {}% system, {}% intr, {}% idle ",
+                cpu.user * 100.0, cpu.nice * 100.0, cpu.system * 100.0, cpu.interrupt * 100.0, cpu.idle * 100.0);
+        },
+        Err(x) => println!("\nCPU load: error: {}", x)
+    };
+
     match sys.cpu_temp() {
-        Ok(cpu_temp) => Ok(cpu_temp),
-        Err(x) => println!("CPU temp: {}", x)
+        Ok(cpu_temp) => println!("\nCPU temp: {}", cpu_temp),
+        Err(x) => println!("\nCPU temp: {}", x)
     }
 
 }
+
 
 fn main() {
     sensor_data()
