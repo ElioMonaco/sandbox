06be093ff014bfa833e9cc63f2673a7a57957be6
Elio Monaco <elio.monaco@proton.me>
2023-10-30 23:12:54 +0100

    fix views

--- telebot/sql_code/views/chats_dimension.sql
+++ telebot/sql_code/views/chats_dimension.sql
@@ -1,12 +1,26 @@
 CREATE ALGORITHM = UNDEFINED DEFINER = `elio`@` % ` SQL SECURITY DEFINER VIEW `chats_dimension` AS
 
-SELECT DISTINCT `chats`.`chat_id` AS `chat_id`
-	,(
-		CASE 
-			WHEN (`chats`.`title` IS NOT NULL)
-				THEN `chats`.`title`
-			ELSE `chats`.`username`
-			END
-		) AS `chat_name`
-	,`chats`.`type` AS `chat_type`
-FROM `chats`
+WITH cte1 AS (
+    SELECT DISTINCT `chats`.`chat_id` AS `chat_id`
+        ,(
+            CASE 
+                WHEN (`chats`.`title` IS NOT NULL)
+                    THEN `chats`.`title`
+                ELSE `chats`.`username`
+                END
+            ) AS `chat_name`
+        ,`chats`.`type` AS `chat_type`
+        ,`chats`.`valid_from` AS `valid_from`
+    FROM `chats`
+)
+,cte2 AS (
+    SELECT *
+            ,ROW_NUMBER() OVER(PARTITION BY `chat_id` ORDER BY `valid_from` DESC) AS row_num  
+    FROM cte1
+)
+
+SELECT `chat_id`
+        ,`chat_name`
+        ,`chat_type`
+FROM cte2
+WHERE row_num = 1
--- telebot/sql_code/views/users_dimension.sql
+++ telebot/sql_code/views/users_dimension.sql
@@ -1,5 +1,13 @@
 CREATE ALGORITHM = UNDEFINED DEFINER = `elio`@` % ` SQL SECURITY DEFINER VIEW `users_dimension` AS
 
-SELECT DISTINCT `users`.`user_id` AS `user_id`
-	,`users`.`username` AS `username`
-FROM `users`
+WITH cte AS (
+    SELECT DISTINCT `users`.`user_id` AS `user_id`
+        ,`users`.`username` AS `username`
+        ,ROW_NUMBER() OVER(PARTITION BY `user_id` ORDER BY `valid_from` DESC) AS row_num  
+    FROM `users`
+)
+
+SELECT `user_id`
+        ,`username`
+FROM cte
+WHERE row_num = 1
